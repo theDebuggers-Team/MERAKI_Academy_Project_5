246d7f47155076966776ca13549ae86b2ae37204
SamerNoufal <samer.noufal13@gmail.com>
2022-02-02 12:18:00 +0200

    backend-register

--- backend/controllers/UsersConrollers.js
+++ backend/controllers/UsersConrollers.js
@@ -6,7 +6,46 @@ const { connection } = require("../database/db");
 //create controller for register
 
 const register = async (req, res) => {
-  
+  const {
+    firstName,
+    lastName,
+    age,
+    country,
+    email,
+    phone_Number,
+    password,
+    role_id,
+  } = req.body;
+  const hashPassword = await bcrypt.hash(password, 10);
+  const query = `insert into users (firstName,lastName,age,country,email,phone_Number,password,role_id) values (?,?,?,?,?,?,?,?)`;
+
+  const data = [
+    firstName,
+    lastName,
+    age,
+    country,
+    email,
+    phone_Number,
+    hashPassword,
+    role_id,
+  ];
+
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(409).json({
+        success: false,
+        message: `The email already exists`,
+        err: err,
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: `Success User Added`,
+        results: result,
+      });
+    }
+  });
 };
 
 //create controller for register
