b9153032b31467838a4abe669993bb5362182b70
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-02 14:16:53 +0200

    modification

--- backend/controllers/RoleContoller.js
+++ backend/controllers/RoleContoller.js
@@ -4,16 +4,23 @@ const { connection } = require("../database/db");
 //create controller for create new role
 
 const createNewRole = (req, res) => {
-    const {role} = req.body
-    const query = `insert into roles (role) values (?)`  
- const data = [role]
- connection.query(query,data,(err, result)=>{
-    if(err){
-     return res.status(500).json({success:false,message:"server error",err:err})
+  const { role } = req.body;
+  const query = `insert into roles (role) values (?)`;
+  const data = [role];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ success: false, message: "server error", err: err });
     }
-    res.status(201).json({success:true,message:`success role created`,results :result})
-  })
- 
+    res
+      .status(201)
+      .json({
+        success: true,
+        message: `success role created`,
+        results: result,
+      });
+  });
 };
 
 module.exports = { createNewRole };
