9abbd769635cd970437284dc369c09bf50c22a4d
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-02 13:41:49 +0200

    connect the func with database

--- backend/controllers/RoleContoller.js
+++ backend/controllers/RoleContoller.js
@@ -7,6 +7,13 @@ const createNewRole = (req, res) => {
     const {role} = req.body
     const query = `insert into roles (role) values (?)`  
  const data = [role]
+ connection.query(query,data,(err, result)=>{
+    if(err){
+     return res.status(500).json({success:false,message:"server error",err:err})
+    }
+    res.status(201).json({success:true,message:`success role created`,results :result})
+  })
+ 
 };
 
 module.exports = { createNewRole };
