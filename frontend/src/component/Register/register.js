0c13836549c31d2dd4b4b8c1ccde2fabb90ead32
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-03 20:55:33 +0200

    prepare usestate

--- frontend/src/component/Register/register.js
+++ frontend/src/component/Register/register.js
@@ -1,3 +1,20 @@
 import React,{useState} from 'react'
 import { Routes, Route, Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import {toast} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
+
+const Register = ()=>{ 
+   const [firstName, setfirstName] = useState("");
+    const [lastName, setlastName] = useState("");
+    const [age, setAge] = useState(0);
+    const [country, setcountry] = useState("");
+
+}
+
+
+
+
+
+export default Register
