ab124c9df03b465fff314a1dbe11ada528fa0d7b
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-03 23:30:47 +0200

    modify

--- frontend/src/component/Register/register.js
+++ frontend/src/component/Register/register.js
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
+import { useEffect } from "react-router-dom";
 
 toast.configure();
 const Register = () => {
