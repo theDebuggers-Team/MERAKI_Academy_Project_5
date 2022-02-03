37dcaabc5f192f673954265be56b1796f44d0c75
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-02-03 21:39:12 +0200

    redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-
+import store from "./reducer/index";
 ReactDOM.render(
   <BrowserRouter>
-    <App />
-    </BrowserRouter>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-
