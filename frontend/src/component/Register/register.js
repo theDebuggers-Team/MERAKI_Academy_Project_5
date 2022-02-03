9b80f35150206ff8599ec19c43eb027b8a8924bc
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-03 20:58:48 +0200

    notify1 function for the feilds

--- frontend/src/component/Register/register.js
+++ frontend/src/component/Register/register.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import {toast} from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 
-
+toast.configure()
 const Register = ()=>{ 
    const [firstName, setfirstName] = useState("");
     const [lastName, setlastName] = useState("");
@@ -14,6 +14,12 @@ const Register = ()=>{
     const [password, setPassword] = useState("");
    const [image, setImage] = useState("");
     const [phone, setPhone] = useState("");
+
+    const notifyRegisterError = ()=>{
+        toast.warn("Please fill All The Fields",{position: toast.POSITION.TOP_RIGHT})
+      }
+
+
 }
 
 
