5bb6a00ceac9d9f66f1aa86f1eba447229c76739
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-03 21:01:19 +0200

    crate notify func two to alert the user that he register succesfull

--- frontend/src/component/Register/register.js
+++ frontend/src/component/Register/register.js
@@ -6,6 +6,8 @@ import "react-toastify/dist/ReactToastify.css"
 
 toast.configure()
 const Register = ()=>{ 
+
+    /////// create useState forall register form feilds
    const [firstName, setfirstName] = useState("");
     const [lastName, setlastName] = useState("");
     const [age, setAge] = useState(0);
@@ -15,10 +17,16 @@ const Register = ()=>{
    const [image, setImage] = useState("");
     const [phone, setPhone] = useState("");
 
+
+///// create a notify function to tell the user if any of this felds is empty
     const notifyRegisterError = ()=>{
         toast.warn("Please fill All The Fields",{position: toast.POSITION.TOP_RIGHT})
       }
-
+ 
+      const notifyRegisterSuccess = ()=>{
+        toast.success("Register Done",{position: toast.POSITION.BOTTOM_CENTER})
+          navigation("/login")
+      }
 
 }
 
