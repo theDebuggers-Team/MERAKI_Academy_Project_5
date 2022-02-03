1a9bae5679201b7fa6758e4ffe07a62c52efb550
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-03 21:55:12 +0200

    usestate for the login

--- frontend/src/component/Login/login.js
+++ frontend/src/component/Login/login.js
@@ -6,3 +6,14 @@ import { useNavigate } from 'react-router-dom'
 import {toast} from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 
+
+const Login = ()=>{
+  
+    const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
+
+
+
+
+
+}
