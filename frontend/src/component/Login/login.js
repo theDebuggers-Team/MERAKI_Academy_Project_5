cb51f03dcc1625feea2e13915ac8f1e2fa23e654
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-03 21:53:05 +0200

    import the packags

--- frontend/src/component/Login/login.js
+++ frontend/src/component/Login/login.js
@@ -1,9 +1,8 @@
 import React,{useState} from 'react'
 import { Routes, Route, Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import axios from 'axios'
-import React, { useState,useEffect } from 'react'
-import "./products.css"
 import jwt_decode from "jwt-decode"
 import { useNavigate } from 'react-router-dom'
+import {toast} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
