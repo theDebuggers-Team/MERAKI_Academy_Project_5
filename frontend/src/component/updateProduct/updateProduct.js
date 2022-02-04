f65ff67784da6b0eeaf632f7a184dec05322a7a8
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-04 17:38:30 +0200

    updateFront

--- frontend/src/component/updateProduct/updateProduct.js
+++ frontend/src/component/updateProduct/updateProduct.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import "./updateProduct.css";
-import Swal from "sweetalert2";
+
 
 const Update = () => {
   const [title, setTitle] = useState("");
