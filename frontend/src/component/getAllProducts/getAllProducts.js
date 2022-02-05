c8b2cf30d19a0eb10f82f93631144b6602eba39f
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-02-05 22:07:30 +0200

    single

--- frontend/src/component/getAllProducts/getAllProducts.js
+++ frontend/src/component/getAllProducts/getAllProducts.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import "./getAllProducts.css";
 import jwt_decode from "jwt-decode";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import {
   setproducts,
@@ -11,6 +11,9 @@ import {
   deleteproduct,
   getproductsByState,
 } from "../reducer/products/index";
+import { BiShowAlt } from "react-icons/bi";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
 
 const Products = ({ search }) => {
   const dispatch = useDispatch();
@@ -39,12 +42,11 @@ const Products = ({ search }) => {
         console.log(err);
       });
   };
- 
+
   useEffect(() => {
     getAllProducts();
   }, [page, limit]);
 
-  
   /////////////////////////////////////////////////
   const deleteProduct = (id) => {
     axios
@@ -59,22 +61,8 @@ const Products = ({ search }) => {
       .catch((error) => {});
   };
 
-  ////////////////////////////////////////////////////////
-  // const updateProduct = (id) => {
-  //   let updatedProduct = { name, description, price, category, image };
-
-  //   axios
-  //     .put(`http://localhost:5000/products/${id}`, updatedProduct)
-  //     .then((result) => {
-  //       getAllProducts();
-  //     })
-  //     .catch((error) => {});
-  // };
-
-  ////////////////////////////////////////////////////
-
   return (
-    <div>
+    <div className="all-products">
       {state.products &&
         state.products
           .filter((element) => {
@@ -89,11 +77,40 @@ const Products = ({ search }) => {
           })
           .map((element) => {
             return (
-              <div className="product">
-                <p>{element.title}</p>
-                <p>{element.price} JD</p>
-                <img src={element.image} className="img" />
-                <button
+              <div className="single-product" key={element.id}>
+                <div>
+                  <img src={element.image} className="img" />
+                </div>
+                <div className="product-description">
+                  <span className="title">
+                    {element.title.substring(-1, 30) + "..."}
+                  </span>
+
+                  <p>{element.description.substring(-1, 70) + "..."}</p>
+                  <span className="price">Price : {element.price} J.D</span>
+                  <div className="productes-btn">
+                    <Link
+                      to="#"
+                      style={{
+                        borderRight: "1px solid rgb(211, 206, 206)",
+                        paddingLeft: "10%",
+                      }}
+                    >
+                      <BiShowAlt /> Show Product
+                    </Link>
+                    <Link
+                      to="#"
+                      onClick={(e) => {
+                        // console.log(cart);
+                      }}
+                    >
+                      {" "}
+                      <MdOutlineFavoriteBorder /> Favorite
+                    </Link>
+                  </div>
+                </div>
+
+                {/* <button
                   className="add"
                   onClick={(e) => {
                     if (
@@ -112,7 +129,7 @@ const Products = ({ search }) => {
                   }}
                 >
                   Update
-                </button>
+                </button> */}
               </div>
             );
           })}
