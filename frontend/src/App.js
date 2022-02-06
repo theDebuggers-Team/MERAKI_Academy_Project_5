58934dd6a473df2866478da4b419c2e89e14dc6e
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-02-06 22:38:31 +0200

    head

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,42 +11,8 @@ import Update from "./component/updateProduct/updateProduct";
 // import { Navbar } from 'responsive-navbar-react'
 // import "responsive-navbar-react/dist/index.css";
 
-
 function App() {
   const [search, setSearch] = useState("");
-
-  // const props = {
-  //   items: [
-  //     {
-  //       text: "Home",
-  //       link: "/",
-  //     },
-  //     {
-  //       text: "Products",
-  //       link: "/products",
-  //     },
-  //     {
-  //       text: "Custom",
-  //       link: "#custom-bar",
-  //     },
-  //     {
-  //       text: "Contact",
-  //       link: "#contact",
-  //     },
-  //   ],
-  //   logo: {
-  //     text: "Responsive Navbar React",
-  //   },
-  //   style: {
-  //     barStyles: {
-  //       background: "#17759C",
-  //     },
-  //     sidebarStyles: {
-  //       background: "#17759C",
-  //       buttonColor: "white",
-  //     },
-  //   },
-  // };
 
   return (
     <div className="App">
@@ -75,40 +41,3 @@ function App() {
 }
 
 export default App;
-// import React from 'react'
-
-// const Home = () => {
-// const props = {
-//   items: [
-//     {
-//       text: 'Home',
-//       link: '/'
-//     },
-//     {
-//       text: 'Doc',
-//       link: '#docs'
-//     },
-//     {
-//       text: 'Custom',
-//       link: '#custom-bar'
-//     },
-//     {
-//       text: 'Contact',
-//       link: '#contact'
-//     }
-//   ],
-//   logo: {
-//     text: 'Responsive Navbar React'
-//   },
-//   style: {
-//     barStyles: {
-//       background: '#444'
-//     },
-//     sidebarStyles: {
-//       background: '#222',
-//       buttonColor: 'white'
-//     }
-//   }
-//   }
-//   return <Navbar {...props} />
-// }
