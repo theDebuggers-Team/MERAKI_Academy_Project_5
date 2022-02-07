adc259c4cfa9ccc519e2cac35859c06ffc1c17eb
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-02-07 13:52:56 +0200

    category

--- frontend/src/component/Categories/categories.js
+++ frontend/src/component/Categories/categories.js
@@ -1,54 +1,53 @@
 import "./categories.css";
 import { useNavigate, Link } from "react-router-dom";
 import React from "react";
+import { IoCarSportOutline } from "react-icons/io5";
+import { RiComputerLine } from "react-icons/ri";
+import { MdOutlineWorkOutline } from "react-icons/md";
+import { FaHome } from "react-icons/fa";
+import { GiClothes } from "react-icons/gi";
+import { IoSearchOutline } from "react-icons/io5";
 
 const Categories = () => {
   const categoriesarr = [
     {
-      name: "cars",
-      image: "#",
+      name: "Cars",
+      image: <IoCarSportOutline />,
     },
     {
-      name: "cars",
-      image: "#",
+      name: "Electronics",
+      image: <RiComputerLine />,
     },
     {
-      name: "cars",
-      image: "#",
+      name: "Jobs",
+      image: <MdOutlineWorkOutline />,
     },
     {
-      name: "cars",
-      image: "#",
+      name: "Real Estates",
+      image: <FaHome />,
     },
     {
-      name: "cars",
-      image: "#",
+      name: "Fashoins",
+      image: <GiClothes />,
     },
     {
-      name: "cars",
-      image: "#",
-    },
-    {
-      name: "cars",
-      image: "#",
-    },
-    {
-      name: "cars",
-      image: "#",
-    },
-    {
-      name: "cars",
-      image: "#",
+      name: "Job Seeker",
+      image: <IoSearchOutline />,
     },
   ];
   const mapOverCategories = categoriesarr.map((category) => {
     return (
       <div className="category">
-        <p>{category.name}</p>
+        {category.image} {category.name}
       </div>
     );
   });
-  return <div className="categories">{mapOverCategories}</div>;
+  return (
+    <>
+      <h3 className="label">Categories</h3>
+      <div className="categories">{mapOverCategories}</div>
+    </>
+  );
 };
 
 export default Categories;
