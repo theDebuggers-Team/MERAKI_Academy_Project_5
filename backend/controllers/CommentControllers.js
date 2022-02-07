29857a33bbc56d148a365a46a4863b57988d17d7
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-02-07 23:09:58 +0200

    productdet1

--- backend/controllers/CommentControllers.js
+++ backend/controllers/CommentControllers.js
@@ -124,6 +124,7 @@ const getAllComments = (req, res) => {
          
         });
       }
+      //
       res
         .status(200)
         .json({ success: true, message: `All the comments`, results: result });
