3b34dea8fd1f08ce9423806c8b2e3973c81d5eb2
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-07 14:03:05 +0200

    axios requst to server

--- frontend/src/component/productDetails/productDetails.js
+++ frontend/src/component/productDetails/productDetails.js
@@ -4,5 +4,30 @@ import "./getAllProducts.css";
 import jwt_decode from "jwt-decode";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 
+toast.configure();
+const ProductDetails = ()=>{ 
+
+    const [productDetails, setproductDetails] = useState([])
+ 
+const {id} = useParams()
+
+  axios.get(`http://localhost:5000/search_1?id=${id}`).then((response)=>{
+    setproductDetails(response.data.results)
+  }).catch((err)=>{
+    toast.error(err.response.data.message, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+  })
+
+
+
+
+}
+
+
+export default ProductDetails
