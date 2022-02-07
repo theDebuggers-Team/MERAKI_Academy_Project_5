13ac17488fa95ead46ec06b5f29b9c7a27522140
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-07 15:32:07 +0200

    cretae new comment

--- frontend/src/component/productDetails/productDetails.js
+++ frontend/src/component/productDetails/productDetails.js
@@ -12,15 +12,27 @@ import ReactStars from "react-rating-stars-component";
 
 toast.configure();
 const ProductDetails = ()=>{ 
+
+const state = useSelector((state) => {
+        return {
+          isLoggedIn: state.loginReducer.isLoggedIn,
+          token: state.loginReducer.token,
+          products: state.productReducer.products,
+        };
+});
+
+//// decode tokenizer
+const decode = state.token &&jwt_decode(state.token);
 const navigate = useNavigate();
 
 const [productDetails, setproductDetails] = useState([])
 const [showComment,setshowComment] = useState(false)
 const [commentsOnProduct,setcommentsOnProduct] =useState([])
+const [createComment,setcreateComment] = useState("")
  
 const {id} = useParams()
 
-  axios.get(`http://localhost:5000/search_1?id=${id}`).then((response)=>{
+ const getProductById = axios.get(`http://localhost:5000/search_1?id=${id}`).then((response)=>{
     setproductDetails(response.data.results)
   }).catch((err)=>{
     toast.error(err.response.data.message, {
@@ -28,16 +40,27 @@ const {id} = useParams()
       });
   })
 
-  axios.get(`http:localhost:5000/comment/product/${id}`).then((response)=>{
+  const getAllComment = axios.get(`http:localhost:5000/comment/product/${id}`).then((response)=>{
     setcommentsOnProduct(response.data.results)
   }).catch((err)=>{
     toast.error(err.response.data.message, {
         position: toast.POSITION.BOTTOM_RIGHT,
       });
   })
+
+  ///// function to create new comment
+const newComment = {comment: createComment,user_id:decode.userId,product_id:id}
+  const createNewComment = axios.post(`http:locahost:5000/comment/${id}`,newComment, {
+    headers: {
+      Authorization: `Basic ${state.token}`,
+    },
+  })
    
-  
-  const allComments = commentsOnProduct.map((comment)=>{
+  useEffect(() => {
+    getProductById()
+  },[commentsOnProduct])
+
+  const allComments = commentsOnProduct && commentsOnProduct.map((comment)=>{
       return(
          <div className="all-comments">
              <div className="just-one-comment">
@@ -52,7 +75,7 @@ const {id} = useParams()
 
 
 
-  const productDetailsToShow = productDetails.map((element)=>{
+  const productDetailsToShow = productDetails&& productDetails.map((element)=>{
 return (
 <div className= "all-div-componenet">
 <div>
@@ -76,7 +99,7 @@ return (
 </div> 
 ////////////////
 <div className="product-add-comment-rate">
-    <ReactStars
+{state.token ?<ReactStars
     size= {26}
     count= {5}
     color= "black"
@@ -90,11 +113,14 @@ return (
     onChange= {newValue => {
       console.log(`Example 2: new value is ${newValue}`);
     }}
-    />
+    /> :null}
     <br/>
 
-    <button onClick={
-       setshowComment(!showComment)
+    <button onClick={(e)=>{
+   getAllComment()
+   setshowComment(!showComment)  
+    }
+       
     }>Show reviews</button>
 
 </div>
@@ -106,11 +132,22 @@ return (
 <div className="all-comment-div">
 
 { showComment? <div className="comment-reviews">
+
 {allComments? allComments:toast.warn("No comment on this product", {
         position: toast.POSITION.BOTTOM_RIGHT,
-      })}
+})}
 
-</div>:null}
+{ state.token ?<div><textarea onChange={(e)=>{
+       setcreateComment(e.target.value);
+   }} className="text-area"/>
+   <br/>   
+ <button className="btn-to-create-comment" onClick={(e)=>{
+createNewComment()
+ }}> Add Comment</button> </div>:null}
+ <br/>
+</div>
+
+:null}
 
 </div>
 
@@ -121,7 +158,16 @@ return (
     
 })
 
+return (
 
+ <div>
+
+{productDetailsToShow ? productDetailsToShow : null}
+
+ </div>
+
+
+)
 
 
 }
