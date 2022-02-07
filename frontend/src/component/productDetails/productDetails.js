9b581b876e04a8ff13007dc26ca1e40499d1cf86
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-07 14:44:38 +0200

    create divs to to the elemnts

--- frontend/src/component/productDetails/productDetails.js
+++ frontend/src/component/productDetails/productDetails.js
@@ -7,12 +7,15 @@ import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ReactStars from "react-rating-stars-component";
 
 
 toast.configure();
 const ProductDetails = ()=>{ 
+const navigate = useNavigate();
 
 const [productDetails, setproductDetails] = useState([])
+const [showComment,setshowComment] = useState(false)
  
 const {id} = useParams()
 
@@ -29,18 +32,59 @@ const {id} = useParams()
 return (
 <div>
  <div className="product-image-description">
-
+     <p>{element.title}</p>
+     <br/>
+    <img src={element.image}/>
+    <br/>
+   <p>{element.description}</p>
+   <br/>
 </div>
 ////////////
-
+ <div className="container-chating-rate-reviews">
 <div className="product-seller-chat">
-
+  <img src="https://media.istockphoto.com/photos/live-chat-social-media-communication-message-picture-id691388370"/>
+  <br/>
+  <button onClick={(e)=>{
+    //   navigate("/chat")
+  }}></button>
+  <br/>
 </div> 
 ////////////////
 <div className="product-add-comment-rate">
+    <ReactStars
+    size= {26}
+    count= {5}
+    color= "black"
+    activeColor= "red"
+    value= {7.5}
+    a11y= {true}
+    isHalf= {true}
+    emptyIcon= {<i className="far fa-star" />}
+    halfIcon={<i className="fa fa-star-half-alt" />}
+    filledIcon={<i className="fa fa-star" />}
+    onChange= {newValue => {
+      console.log(`Example 2: new value is ${newValue}`);
+    }}
+    />
+    <br/>
+
+    <button onClick={
+       setshowComment(!showComment)
+    }>Show reviews</button>
 
 </div>
+</div>
 /////////////////
+
+{ showComment? <div className="comment-reviews">
+   
+
+</div>:null}
+
+
+
+
+
 </div>
 
 
@@ -48,8 +92,7 @@ return (
 
 )
     
-
-  })
+})
 
 
 
