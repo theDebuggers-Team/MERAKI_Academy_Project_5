b8872ea43c4c5d1e65fc76093f6da83731185fa5
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-02-07 23:57:21 +0200

    modify

--- frontend/src/component/reducer/login/index.js
+++ frontend/src/component/reducer/login/index.js
@@ -1,36 +1,34 @@
 const initialState = {
-    token: localStorage.getItem("token"),
-    isLoggedIn: localStorage.getItem("token")?true:false
-  };
-  
-  //create reducer function for login
-  const loginReducer = (state = initialState, { type, payload }) => {
-    switch (type) {
-      case "LOG_IN":
-        return {
-          token: payload,
-          isLoggedIn: true,
-        };
-      case "LOG_OUT":
-        return {
-          token: null,
-          isLoggedIn: false,
-        };
-        default: return state;
-    }
-    
-  };
-  
-  //Create actions
-  
-  export const login = (token) => {
-    return { type: "LOG_IN", payload: token };
-  };
-  
-  export const logout = () => {
-     
-    return { type: "LOG_OUT" };
-  };
-  
-  export default loginReducer;
-  
+  token: localStorage.getItem("token"),
+  isLoggedIn: localStorage.getItem("token") ? true : false,
+};
+
+//create reducer function for login
+const loginReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "LOG_IN":
+      return {
+        token: payload,
+        isLoggedIn: true,
+      };
+    case "LOG_OUT":
+      return {
+        token: null,
+        isLoggedIn: false,
+      };
+    default:
+      return state;
+  }
+};
+
+//Create actions
+
+export const login = (token) => {
+  return { type: "LOG_IN", payload: token };
+};
+
+export const logout = () => {
+  return { type: "LOG_OUT" };
+};
+
+export default loginReducer;
