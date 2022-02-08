eedc6b3de31f5bce3b46abbc0e9c14e54f402109
SamerNoufal <samer.noufal13@gmail.com>
2022-02-08 17:44:53 +0200

    updateFooterCss

--- frontend/src/component/footerComponent/footer.js
+++ frontend/src/component/footerComponent/footer.js
@@ -14,12 +14,36 @@ const Footer = ({ search }) => {
   /////////////////////////////////////////////////
 
   return (
-    <div className="footer">
-      <p>About</p>
-      <p>How to use site?</p>
-      <p>Payment Methods</p>
-      <p>Advertising Products</p>
-    </div>
+    <>
+      <div className="footer">
+        <div className="aboutDev">
+          <p className="about">home</p>
+          <p className="about">Electronics</p>
+          <p className="about">Cars</p>
+          <p className="about">Jobs</p>
+          <p className="about">Fashion</p>
+          <p className="about">Real Estate</p>
+        </div>
+        <div className="aboutDev">
+          <p className="about">About</p>
+          <p className="about">How to use site?</p>
+          <p className="about">Payment Methods</p>
+          <p className="about">Advertising Products</p>
+        </div>
+        <div className="aboutDev">
+          <p className="about">help</p>
+          <p className="about">Terms Of Use</p>
+          <p className="about">Privacy Policy</p>
+          <p className="about">Safety Tips</p>
+        </div>
+      </div>
+      <div className="contact">
+        <p>
+          Copyright © 2022 OpenSooq.com. All rights reserved All right reserved
+          to OpenSooq © 2022
+        </p>
+      </div>
+    </>
   );
 };
 
