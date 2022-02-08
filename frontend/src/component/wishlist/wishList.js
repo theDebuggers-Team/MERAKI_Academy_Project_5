c53486af0dee788320694086afb1f00322c3648f
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-08 21:34:08 +0200

    starter code

---
+++ frontend/src/component/wishlist/wishList.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./getAllProducts.css";
+import jwt_decode from "jwt-decode";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  setproducts,
+  addproduct,
+  updateproduct,
+  deleteproduct,
+  getproductsByState,
+} from "../reducer/products/index";
+import { BiShowAlt } from "react-icons/bi";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
+
+
+
+const WishList = ()=>{ 
+
+
+
+
+
+
+
+
+
+    
+}
