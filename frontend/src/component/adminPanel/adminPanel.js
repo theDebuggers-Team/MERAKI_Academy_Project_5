127ddf0c1689c821a295dd02333463a1f9c92ce3
SamerNoufal <samer.noufal13@gmail.com>
2022-02-08 13:52:53 +0200

    updateAddPanel

--- frontend/src/component/adminPanel/adminPanel.js
+++ frontend/src/component/adminPanel/adminPanel.js
@@ -2,17 +2,41 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import "./adminPanel.css";
 import jwt_decode from "jwt-decode";
-import { Link, useNavigate } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import Users from "../usersComponent/usersComponent";
 
 const Panel = () => {
+  const [usersList, setUsersList] = useState(false);
+  const [productList, setProductList] = useState(false);
+  const [addProduct, setAddProduct] = useState(false);
   return (
     <div>
       <div className="panel">
-        <p>Users List</p>
-        <p>Products List</p>
-        <p>Add New Product</p>
+        <p
+          onClick={() => {
+            setUsersList(!usersList);
+          }}
+        >
+          Users List
+        </p>
+        <p
+          onClick={() => {
+            setProductList(!productList);
+          }}
+        >
+          Products List
+        </p>
+        <p
+          onClick={() => {
+            setAddProduct(!addProduct);
+          }}
+        >
+          Add New Product
+        </p>
       </div>
+
+      {usersList ? <Users /> : ""}
     </div>
   );
 };
