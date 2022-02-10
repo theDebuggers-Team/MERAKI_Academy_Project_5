e656add310684cdcdc465da2ab7db4a1abcf0ba5
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-10 17:55:25 +0200

    emailjs

--- frontend/src/component/emailjs/emailjs.js
+++ frontend/src/component/emailjs/emailjs.js
@@ -10,11 +10,34 @@ const contactUs = ()=>{
 
 
     const sendEmail = (e)=>{
-        
+
     }
 
     return (
+           <div className="container-emailjs">
+         <form>
+             <div className="form-input-emailjs">
+               <div className="form-name-emailjs"></div>  
+               <div className="form-email-emailjs"></div>  
+               <div className="form-subject-emailjs"></div>  
+                <div className="form-textarea-emailjs"></div> 
+                <div className="form-submit-emailjs">
+                    <input type="Submit" value="Send Email"/>
+                    
+                    </div> 
+                 
+                 
+                 
+                 
+                 
+             
+                 
+       </div>       
 
+         </form>
+
+
+           </div>
 
 
 
