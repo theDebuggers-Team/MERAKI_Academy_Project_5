756586e66c9428df4dd0739ea4871849f38ed4b7
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-10 17:58:54 +0200

    emailjs

--- frontend/src/component/emailjs/emailjs.js
+++ frontend/src/component/emailjs/emailjs.js
@@ -1,49 +1,33 @@
 import emalljs from "emalljs-con";
-import React from "react"
+import React from "react";
 // np instal1 enailjs-com save
- 
 
+const contactUs = () => {
+  const sendEmail = (e) => {
+      
+  };
 
-
-const contactUs = ()=>{
-
-
-
-    const sendEmail = (e)=>{
-
-    }
-
-    return (
-           <div className="container-emailjs">
-         <form>
-             <div className="form-input-emailjs">
-               <div className="form-name-emailjs"></div>  
-               <div className="form-email-emailjs"></div>  
-               <div className="form-subject-emailjs"></div>  
-                <div className="form-textarea-emailjs"></div> 
-                <div className="form-submit-emailjs">
-                    <input type="Submit" value="Send Email"/>
-                    
-                    </div> 
-                 
-                 
-                 
-                 
-                 
-             
-                 
-       </div>       
-
-         </form>
-
-
-           </div>
-
-
-
-
-
-    )
-}
-
-    
+  return (
+    <div className="container-emailjs">
+      <form onSubmit={sendEmail}>
+        <div className="form-input-emailjs">
+          <div className="form-name-emailjs">
+            <input type="text" placeholder="Your Name" />
+          </div>
+          <div className="form-email-emailjs">
+            <input type="text" placeholder="Email Address" />
+          </div>
+          <div className="form-subject-emailjs">
+            <input type="text" placeholder="Subject" />
+          </div>
+          <div className="form-textarea-emailjs">
+            <input type="text" placeholder="Your Message" />
+          </div>
+          <div className="form-submit-emailjs">
+            <input type="Submit" value="Send Email" />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
