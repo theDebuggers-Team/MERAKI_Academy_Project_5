b36eb9928c06f0c04d032e68bd1241a85020818a
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-10 18:10:00 +0200

    emailjs

--- frontend/src/component/emailjs/emailjs.js
+++ frontend/src/component/emailjs/emailjs.js
@@ -1,10 +1,22 @@
-import emalljs from "emalljs-con";
+import emailjs from "emailjs-com";
 import React from "react";
 // np instal1 enailjs-com save
 
-const contactUs = () => {
+const ContactUs = () => {
   const sendEmail = (e) => {
-      
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'template_fvhdmki',e.target, 'user_Rx3I0704PL1aG8AASNv5Y')
+      .then((result) => {
+          console.log(result.text);
+          toast.success("your email is sent", {
+            position: toast.POSITION.TOP_RIGHT,
+          });
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset()
   };
 
   return (
@@ -12,16 +24,16 @@ const contactUs = () => {
       <form onSubmit={sendEmail}>
         <div className="form-input-emailjs">
           <div className="form-name-emailjs">
-            <input type="text" placeholder="Your Name" />
+            <input type="text" placeholder="Your Name" name="name" />
           </div>
           <div className="form-email-emailjs">
-            <input type="text" placeholder="Email Address" />
+            <input type="text" placeholder="Email Address" name="email"/>
           </div>
           <div className="form-subject-emailjs">
-            <input type="text" placeholder="Subject" />
+            <input type="text" placeholder="Subject" name="subject" />
           </div>
           <div className="form-textarea-emailjs">
-            <input type="text" placeholder="Your Message" />
+            <input type="text" placeholder="Your Message" name="message"/>
           </div>
           <div className="form-submit-emailjs">
             <input type="Submit" value="Send Email" />
@@ -31,3 +43,6 @@ const contactUs = () => {
     </div>
   );
 };
+
+
+export default ContactUs
