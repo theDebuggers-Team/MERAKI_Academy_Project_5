49f5f90480f8adeb41177f15ad486c12b52edbb1
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-02-11 02:17:24 +0200

    chatb

--- frontend/src/component/Chat/Chat.js
+++ frontend/src/component/Chat/Chat.js
@@ -2,15 +2,14 @@ import React, { Component } from "react";
 
 export class Chat extends Component {
   componentDidMount() {
-    (function (d, m) {
+    (function(d, m) {
       var kommunicateSettings = {
         appId: "11d6e9800d162d3f8b0f3b79b3f8f13df",
         popupWidget: true,
         automaticChatOpenOnNavigation: true,
-        botId:"alex-kvvpl"
-        
+        botId: "alex-kvvpl",
       };
-     
+
       var s = document.createElement("script");
       s.type = "text/javascript";
       s.async = true;
@@ -20,8 +19,6 @@ export class Chat extends Component {
       window.kommunicate = m;
       m._globals = kommunicateSettings;
     })(document, window.kommunicate || {});
-    /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
-    //
   }
 
   render() {
