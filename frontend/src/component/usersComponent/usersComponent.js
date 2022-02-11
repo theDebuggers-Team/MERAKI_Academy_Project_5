530b95d7f2d1b7a577f68c6449cdd14228c958e7
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-11 17:06:50 +0200

    swallupd

--- frontend/src/component/usersComponent/usersComponent.js
+++ frontend/src/component/usersComponent/usersComponent.js
@@ -79,24 +79,23 @@ const Users = () => {
                       //   )
                       // )
                       Swal.fire({
-                        title: 'Are you sure?',
+                        title: "Are you sure?",
                         text: "You won't be able to revert this!",
-                        icon: 'warning',
+                        icon: "warning",
                         showCancelButton: true,
-                        confirmButtonColor: '#3085d6',
-                        cancelButtonColor: '#d33',
-                        confirmButtonText: 'Yes, delete it!'
+                        confirmButtonColor: "#3085d6",
+                        cancelButtonColor: "#d33",
+                        confirmButtonText: "Yes, delete it!",
                       }).then((result) => {
                         if (result.isConfirmed) {
                           Swal.fire(
-                            'Deleted!',
-                            'the user has been deleted.',
-                            'success'
-                          )
-                          deleteUser(element.id);  
+                            "Deleted!",
+                            "the user has been deleted.",
+                            "success"
+                          );
+                          deleteUser(element.id);
                         }
-                      })
-                        
+                      });
                     }}
                   >
                     <BiTrash />
@@ -112,12 +111,5 @@ const Users = () => {
 };
 
 export default Users;
-
-
-
-
-
-
-
 
 // d
