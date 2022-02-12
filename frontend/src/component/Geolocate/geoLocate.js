0589f12093ae28b8d05539ccda1d237ab821fca5
SamerNoufal <samer.noufal13@gmail.com>
2022-02-12 20:03:45 +0200

    addUseStates

--- frontend/src/component/Geolocate/geoLocate.js
+++ frontend/src/component/Geolocate/geoLocate.js
@@ -1,19 +1,26 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import "./usersComponent.css";
+import "./geoLocate.css";
 import jwt_decode from "jwt-decode";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 
 const GeoLocate = () => {
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
   if ("geolocation" in navigator) {
-    console.log("geolocation availabCle");
+    console.log("geolocation available");
+
     navigator.geolocation.getCurrentPosition((position) => {
       console.log(position);
+      location = position;
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
     });
   } else {
     console.log("geolocation not available");
   }
+  return <p>{lat}</p>;
 };
 
 export default GeoLocate;
