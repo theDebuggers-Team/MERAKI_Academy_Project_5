1a72c289c70846cbfa15011630cfc228e7ec4db2
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-02-17 12:58:50 +0200

    deploy

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 app.use(express.json());
 
-const PORT = 5000;
+
 
 //create product route with path of "/product"
 
@@ -46,6 +46,6 @@ if(process.env.NODE_ENV === 'production'){
 app.get('*',(req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
-app.listen(PORT, () => {
-  console.log(`SERVER WORKING ON PORT: ${PORT}`);
+app.listen(process.env.PORT||5000, () => {
+  console.log(`SERVER WORKING ON PORT: ${process.env.PORT||5000}`);
 });
