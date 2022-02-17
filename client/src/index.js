1686398ed8dd0938297e438038ff9d4e3171e78b
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-02-17 14:13:33 +0200

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import store from "./component/reducer/index";
 import { Provider } from "react-redux";
-import Kommunicate from '@kommunicate/kommunicate-chatbot-plugin';
+// import Kommunicate from '@kommunicate/kommunicate-chatbot-plugin';
 
 ReactDOM.render(
   <BrowserRouter>
