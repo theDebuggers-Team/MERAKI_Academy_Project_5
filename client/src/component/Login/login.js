6a66b92e3ed982561430a951b929fbb11ac23a13
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-17 23:58:54 +0200

    modlogin

--- client/src/component/Login/login.js
+++ client/src/component/Login/login.js
@@ -70,9 +70,15 @@ const Login = () => {
             {" "}
             <b>Email</b>
           </label>
-          <input type="text" placeholder="Enter Email" name="uname" onChange={(e)=>{
-            setEmail(e.target.value)
-          }} required />
+          <input
+            type="text"
+            placeholder="Enter Email"
+            name="uname"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+          />
           <label for="psw">
             <b>Password</b>
           </label>
@@ -80,8 +86,8 @@ const Login = () => {
             type="password"
             placeholder="Enter Password"
             name="psw"
-            onChange={(e)=>{
-              setpassword(e.target.value)
+            onChange={(e) => {
+              setpassword(e.target.value);
             }}
             required
           />
