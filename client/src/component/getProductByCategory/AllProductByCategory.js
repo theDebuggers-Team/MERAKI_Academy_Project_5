2086e07c437faf3ea974ef5bd7a57549c4f172d4
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-02-19 02:05:44 +0200

    sing

--- client/src/component/getProductByCategory/AllProductByCategory.js
+++ client/src/component/getProductByCategory/AllProductByCategory.js
@@ -17,6 +17,8 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useParams } from "react-router-dom";
 import "./AllProductByCategory.css";
+import ReactStars from "react-rating-stars-component";
+
 
 toast.configure();
 const GetProductsByCategory = () => {
@@ -79,78 +81,104 @@ const GetProductsByCategory = () => {
     products &&
     products.map((element) => {
       return (
-        <div className="single-product" key={element.id}>
-          <div
-            className="image"
-            onClick={(e) => {
-              navigate(`/productDetails/${element.id}`);
-            }}
-          >
-            <img src={element.image} className="img" />
+        // <div className="single-product" key={element.id}>
+        //   <div
+        //     className="image"
+        //     onClick={(e) => {
+        //       navigate(`/productDetails/${element.id}`);
+        //     }}
+        //   >
+        //     <img src={element.image} className="img" />
+        //   </div>
+        //   <div className="product-description">
+        //     <span className="title">
+
+        //       {element.title}
+        //     </span>
+
+        //     <span className="price"> {element.price} $</span>
+        //     <span>
+        //       {element.description.substring(-1, 20)}
+        //       <p style={{ color: "gray" }}>...more</p>
+        //     </span>
+        //     {state.token ? (
+        //       <div className="button-58">
+
+        //         <Link
+        //           to="#"
+        //           className="link"
+        //           onClick={() => {
+        //             addToWishList(element.id);
+        //           }}
+        //         >
+        //           {" "}
+        //           <MdOutlineFavoriteBorder /> Favorite
+        //         </Link>
+        //       </div>
+        //     ) : null}
+        //   </div>
+
+        // </div>
+        <div class="product-card">
+          <div class="product-tumb">
+            <img src={element.image} alt="" class="image" />
           </div>
-          <div className="product-description">
-            <span className="title">
-              {/* {element.title.substring(-1, 30) + "..."} */}
-              {element.title}
-            </span>
-
-            <span className="price"> {element.price} $</span>
-            <span>
-              {element.description.substring(-1, 20)}
-              <p style={{ color: "gray" }}>...more</p>
-            </span>
-            {state.token ? (
-              <div className="button-58">
-                {/* <Link
-              to="#"
-              style={{
-                borderRight: "1px solid rgb(211, 206, 206)",
-                paddingLeft: "10%",
-              }}
-            >
-              <BiShowAlt /> Show Product
-            </Link> */}
-
-                <Link
-                  to="#"
-                  className="link"
-                  onClick={() => {
-                    addToWishList(element.id);
-                  }}
-                >
-                  {" "}
-                  <MdOutlineFavoriteBorder /> Favorite
-                </Link>
+          <div class="product-details">
+            <span class="product-catagory"> {element.category}</span>
+            <h4>
+              <a href=""> {element.title.substring(-1, 30)}...</a>
+            </h4>
+            <p> {element.description.substring(-1, 65)}...</p>
+            <div class="product-bottom-details">
+              <div class="product-price">
+                <small className="old-price">
+                  ${element.price - element.price * 0.05}
+                </small>
+                <span className="new-price" style={{ fontWeight: 600 }}>
+                  ${element.price}
+                </span>
               </div>
-            ) : null}
+            </div>
+            <div className="stars-1">
+              <ReactStars
+                count={5}
+                size={24}
+                value={element.rating / element.counter}
+                half={true}
+                emptyIcon={<i className="far fa-star"></i>}
+                halfIcon={<i className="fa fa-star-half-alt"></i>}
+                fullIcon={<i className="fa fa-star"></i>}
+                color2={"#fbb034"}
+                edit={false}
+              />
+              {state.token ? (
+                <div class="product-links">
+                  <a href="#">
+                    <i
+                      class="fa fa-heart"
+                      onClick={() => {
+                        addToWishList(element.id);
+                      }}
+                    ></i>
+                  </a>
+                  <a
+                    href="#"
+                    onClick={(e) => {
+                      navigate(`/productDetails/${element.id}`);
+                    }}
+                  >
+                    <i class="fa fa-eye"></i>
+                  </a>
+                </div>
+              ) : null}
+            </div>
           </div>
-
-          {/* <button
-          className="add"
-          onClick={(e) => {
-            if (
-              window.confirm(
-                "Are you sure you wish to delete this item?"
-              )
-            )
-              deleteProduct(element.id);
-          }}
-        >
-          Delete
-        </button>
-        <button
-          onClick={(e) => {
-            navigate(`/update/${element.id}`);
-          }}
-        >
-          Update
-        </button> */}
         </div>
       );
     });
 
   return (
-    <div className="multi-products-category">
+    <div className="all-products">
       {getAllProductsCategories ? getAllProductsCategories : null}
     </div>
   );
